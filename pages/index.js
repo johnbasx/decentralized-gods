95b818de8dc0cfcb2e1352b426d1217272a4909a
user <user@users-iMac.local>
2021-06-09 22:50:25 +0530

    basic page done

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,64 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import React, { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
 
-export default function Home() {
+import { getCuratedPhotos } from "../lib/api";
+import { FaMoon, FaLightbulb } from "react-icons/fa";
+
+export default function Home({ data }) {
+  const [photos, setPhotos] = useState(data);
+  console.log(data);
+
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>Decentralized Gods</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+      <div className="h-full px-0">
+        <div className="flex justify-center items-center px-4 py-8">
+          <img
+            src="./decentralized-gods-logo.png"
+            className="h-10 w-10 object-cover rounded-lg"
+          />
+        </div>
+        <div className="flex flex-col justify-center items-center mb-10">
+          <h1 className="text-3xl lg:text-4xl font-extrabold text-gray-50">
+            Decentralized Gods
+          </h1>
+          <span className="text-gray-700 text-sm">by</span>
 
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+            href="https://rarible.com/user/0xcaf2f9599b41956bdfda9b5cd42c05173355bf07"
+            className="mt-2 bg-gray-100 text-black hover:bg-yellow-400  transition delay-100 rounded-full px-6 py-2"
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <span className="text-lg font-semibold tracking-widest">
+              @JOHN SKY
+            </span>
           </a>
         </div>
-      </main>
+        <div className="flex flex-row flex-wrap justify-center">
+          {photos.map((pic) => (
+            <div className="object-cover p-1">
+              <img
+                src={pic.src.original}
+                alt={pic.photographer}
+                className="h-28 w-28 lg:h-40 lg:w-40 object-cover rounded-lg"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+export async function getServerSideProps() {
+  const data = await getCuratedPhotos();
+
+  return {
+    props: {
+      data,
+    },
+  };
 }
